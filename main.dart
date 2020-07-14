e20301352b1d6aa010463b6b220a6c89b40b7159
jslpdcharan <59231349+jslpdcharan@users.noreply.github.com>
2020-07-14 20:27:21 +0530

    Update main.dart
    Added flip card functionalities

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
@@ -10,13 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FlipCard',
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
       home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
+  var store=[1,2,3,4,5,3,2,2,1,2,3,2];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +44,14 @@ class HomePage extends StatelessWidget {
                 _storeelement(),
                 _storeelement(),
                 _storeelement(),
-
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                _storeelement(),
+                _storeelement(),
+                _storeelement(),
               ],
             ),
             Row(
@@ -57,32 +67,42 @@ class HomePage extends StatelessWidget {
   }
 
   _storeelement() {
-    return GestureDetector(
-      onTap: () {
-        print('button tapped');
-        _display();
-      },
-      child: Container(
-        width: 120.0,
-        height: 80.0,
-        decoration: BoxDecoration(
-            shape: BoxShape.rectangle, border: Border.all(color: Colors.black)),
-        child: Center(
-          child: Text('X'),
+   return FlipCard(
+     direction: FlipDirection.VERTICAL,
+     
+     front: Padding(
+       padding: const EdgeInsets.all(8.0),
+       child: Center(
+         child: (
+         Container(
+               child: Text('hello',textAlign: TextAlign.center,),
+           height: 80.0,
+           width: 120.0,
+           color: Colors.green,
+           //color: Colors.blue,
 
+         )
+         ),
+       ),
+     ),
+     back: Padding(
 
-        ),
-      ),
-    );
+       padding: const EdgeInsets.all(8.0),
+       child: Center(
+         child: (
+         Container(
+           child: Text('bye',textAlign: TextAlign.center,),
+           height: 80.0,
+           width:120.0,
+
+         )
+         ),
+       ),
+     ),
+   );
+
   }
-  _display()
-  {
-    var arr=[1,2,3,4,5,6,7,8,9];
-    var arr1=[9,8,7,6,5,4,3,2,1];
-    for(int i=0;i<arr.length;i++)
-      {
-        arr1[i]=arr[i];
-       return print('$arr1[i]');
-      }
-  }
+
+  // ignore: non_constant_identifier_names
 }
+
